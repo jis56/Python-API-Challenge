6aa7df45ab126ea52c63347f5299b750586d36a0
jis56 <70722196+jis56@users.noreply.github.com>
2020-10-30 14:18:36 -0400

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "099e4e04157121376cff80aef9d04899"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDZpnadHNUYspDxUSDcRODki0QzWj-Fk6U"
+g_key = ""
