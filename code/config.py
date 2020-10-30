20708ca6cf45beb96bfffd1b5495ad659f8074b8
Jingwen Sun <viviensun2009@gmail.com>
2020-10-30 14:17:39 -0400

    final push

--- code/config.py
+++ code/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "099e4e04157121376cff80aef9d04899"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyDZpnadHNUYspDxUSDcRODki0QzWj-Fk6U"
